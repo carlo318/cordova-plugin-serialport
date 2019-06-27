50d122ec4b11e4ba0a1530b1e0f4e5971be74372
郭亮 <2292735470@qq.com>
2019-06-27 16:16:05 +0800

    add list port

--- www/cordova-plugin-serialport.js
+++ www/cordova-plugin-serialport.js
@@ -3,18 +3,53 @@ var exec = require("cordova/exec");
 exports.list = function (success, error) {
   exec(success, error, "cordova-plugin-serialport", "list", []);
 };
-exports.open = function (device, rate, success, error) {
+
+var open = function (device, rate, success, error) {
   exec(success, error, "cordova-plugin-serialport", "open", [device, rate]);
 };
-exports.write = function (device, arrayBuffer, success, error) {
+exports.open = open;
+
+var write = function (device, arrayBuffer, success, error) {
   exec(success, error, "cordova-plugin-serialport", "write", [device, arrayBuffer]);
 };
-exports.writeText = function (device, text, success, error) {
+exports.write = write;
+
+var writeText = function (device, text, success, error) {
   exec(success, error, "cordova-plugin-serialport", "writeText", [device, text]);
 };
-exports.close = function (device, success, error) {
+exports.writeText = writeText;
+
+var close = function (device, success, error) {
   exec(success, error, "cordova-plugin-serialport", "close", [device]);
 };
-exports.register = function (device, success, error) {
+exports.close = close;
+
+var register = function (device, success, error) {
   exec(success, error, "cordova-plugin-serialport", "register", [device]);
+};
+exports.register = register;
+
+exports.bind = function (device) {
+
+
+  this.open = function (rate, success, error) {
+    open(device, rate, success, error);
+  };
+
+  this.write = function (arrayBuffer, success, error) {
+    write(device, arrayBuffer, success, error);
+  };
+
+  this.writeText = function (text, success, error) {
+    writeText(device, text, success, error);
+  };
+
+  this.close = function (success, error) {
+    close(device, success, error);
+  };
+
+
+  this.register = function (success, error) {
+    register(device, success, error);
+  }
 };
