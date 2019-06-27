747f27de138d06bcfd819dba453d9922f1361693
郭亮 <2292735470@qq.com>
2019-06-27 16:18:51 +0800

    add list port

--- www/cordova-plugin-serialport.js
+++ www/cordova-plugin-serialport.js
@@ -4,29 +4,34 @@ exports.list = function (success, error) {
   exec(success, error, "cordova-plugin-serialport", "list", []);
 };
 
-var open = function (device, rate, success, error) {
+function open(device, rate, success, error) {
   exec(success, error, "cordova-plugin-serialport", "open", [device, rate]);
-};
+}
+
 exports.open = open;
 
-var write = function (device, arrayBuffer, success, error) {
+function write(device, arrayBuffer, success, error) {
   exec(success, error, "cordova-plugin-serialport", "write", [device, arrayBuffer]);
-};
+}
+
 exports.write = write;
 
-var writeText = function (device, text, success, error) {
+function writeText(device, text, success, error) {
   exec(success, error, "cordova-plugin-serialport", "writeText", [device, text]);
-};
+}
+
 exports.writeText = writeText;
 
-var close = function (device, success, error) {
+function close(device, success, error) {
   exec(success, error, "cordova-plugin-serialport", "close", [device]);
-};
+}
+
 exports.close = close;
 
-var register = function (device, success, error) {
+function register(device, success, error) {
   exec(success, error, "cordova-plugin-serialport", "register", [device]);
-};
+}
+
 exports.register = register;
 
 exports.bind = function (device) {
